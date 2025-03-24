caf6146d170607fc65a58db55c8d3d9d14c7991c
Jade1724 <haruka.i1724@gmail.com>
2025-03-24 17:53:43 +1300

    fix: fixed build errors.

--- azure-salesforce-app/app/page.tsx
+++ azure-salesforce-app/app/page.tsx
@@ -3,15 +3,20 @@ import { getServerUrl } from "@/utils/server";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
+interface Item {
+  item_id: string,
+  q: number
+}
 export default function Home() {
-  const [data, setData] = useState<any>()
+  const [data, setData] = useState<Item | null>()
 
   const getItem = async () => {
     try {
       const SERVER_URL = await getServerUrl();
-      let res = await axios.get(SERVER_URL + "/items/1?q=testing")
+      const res = await axios.get(SERVER_URL + "/items/1?q=testing")
       setData(res.data)
     } catch (error) {
+      console.error(error)
       setData(null)
     }
   }
